e219f5d30bb3c0c29bd971bc6a5ef911f66b6a2c
Aysenur Tatli <aysenurtatlii@hotmail.com>
2025-02-07 11:19:38 +0300

    change other recipes image size

--- src/components/recipeDetails/OtherRecipes.jsx
+++ src/components/recipeDetails/OtherRecipes.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import { useDispatch, useSelector,} from 'react-redux'
+import { useDispatch, useSelector, } from 'react-redux'
 import { useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router';
 
 function OtherRecipes() {
     const [otherRecipes, setOtherRecipes] = useState([])
-    const {id} = useParams()
+    const { id } = useParams()
     const dispatch = useDispatch();
-    const {recipes} = useSelector((state) => state.recipes);
+    const { recipes } = useSelector((state) => state.recipes);
 
     useEffect(() => {
         const filterRecipes = recipes.filter((r) => r.id !== Number(id));
@@ -17,22 +17,22 @@ function OtherRecipes() {
 
 
 
-  return (
-    <div className='flex flex-col gap-3 border-2 rounded-md p-3 border-prairie-sand-100 my-5'>
-    <h3 className='text-prairie-sand-900 font-bold text-xl'>Other Recipes</h3>
+    return (
+        <div className='flex flex-col gap-3 border-2 rounded-md p-3 border-prairie-sand-100 my-5'>
+            <h3 className='text-prairie-sand-900 font-bold text-xl'>Other Recipes</h3>
 
-    {otherRecipes.map((recipe) => (
-        <Link to={`/recipes/recipe/${recipe.id}`}>
-            <div className='inline-flex gap-3'>
-                <img src={recipe.image} className='w-[100px]' />
-                <h3 className='text-lg text-prairie-sand-900 my-auto'>{recipe.name}</h3>
-            </div>
-        </Link>
+            {otherRecipes.map((recipe) => (
+                <Link to={`/recipes/recipe/${recipe.id}`}>
+                    <div className='inline-flex gap-3'>
+                        <img src={recipe.image} className='size-16 md:size-24' />
+                        <h3 className='text-lg text-prairie-sand-900 my-auto'>{recipe.name}</h3>
+                    </div>
+                </Link>
 
-    ))}
+            ))}
 
-</div>
-  )
+        </div>
+    )
 }
 
 export default OtherRecipes
